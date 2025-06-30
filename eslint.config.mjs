a08ca8fdde867a0ed83d4df63acf649f6b5ffe97
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-06-30 19:14:49 +0100

    Project Init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,27 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    // "eslint:recommended",
+    // "plugin:react/recommended",
+  ),
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  // globalIgnores(["**/generated/"]),
 ];
 
 export default eslintConfig;
+// rules: {
+//   "no-unused-vars": "off",
+//   "@typescript-eslint/no-unused-vars": "warn",
+// },
+
+// eslint.config.js
