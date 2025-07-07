da10f28494509bb8a1c4601583ae337451256c82
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-07-07 13:18:28 +0100

    added error page

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="h-full w-full place-content-center place-items-center">
+      <h1>There was an error!</h1>
+    </div>
+  );
+}
--- src/app/meta-report/page.tsx
+++ src/app/meta-report/page.tsx
@@ -4,41 +4,38 @@ import TierRow from "@/components/TierRow";
 
 export default async function MetaReportPage() {
   //get tl id
-  try {
-    const tl = await prisma.tierlist.findUnique({
-      where: { id: "cmcmmil5j0001hopk1ia9vppb" },
-    });
 
-    if (!tl) return <p>Not found</p>;
+  const tl = await prisma.tierlist.findUnique({
+    where: { id: "cmcmmil5j0001hopk1ia9vppb" },
+  });
 
-    const author = await prisma.user.findUnique({ where: { id: tl.authorId } });
-    const tiers = await tlConstructor(tl);
+  if (!tl) return <p>Not found</p>;
 
-    return !tl ? (
-      <p>No data???</p>
-    ) : (
-      <div className="p-2">
-        <div className="rounded-box border-base-200 bg-base-300 flex flex-col gap-2 border-2 p-2">
-          <div className="pl-1">
-            <h1 className="text-2xl">{tl.name}</h1>
-            <h2 className="text-lg">
-              Author: {author ? author.name : "Unknown"}
-            </h2>
-          </div>
-          {/* Table */}
-          <div className="join join-vertical">
-            {tiers.map((tier, index) => {
-              if (tier.tierName === "?") return;
-              return <TierRow tier={tier} key={index} />;
-            })}
-          </div>
-          <p className="p-1">
-            {tl.descripton ? tl.descripton : "No description..."}
-          </p>
+  const author = await prisma.user.findUnique({ where: { id: tl.authorId } });
+  const tiers = await tlConstructor(tl);
+
+  return !tl ? (
+    <p>No data???</p>
+  ) : (
+    <div className="p-2">
+      <div className="rounded-box border-base-200 bg-base-300 flex flex-col gap-2 border-2 p-2">
+        <div className="pl-1">
+          <h1 className="text-2xl">{tl.name}</h1>
+          <h2 className="text-lg">
+            Author: {author ? author.name : "Unknown"}
+          </h2>
         </div>
+        {/* Table */}
+        <div className="join join-vertical">
+          {tiers.map((tier, index) => {
+            if (tier.tierName === "?") return;
+            return <TierRow tier={tier} key={index} />;
+          })}
+        </div>
+        <p className="p-1">
+          {tl.descripton ? tl.descripton : "No description..."}
+        </p>
       </div>
-    );
-  } catch (error) {
-    return <p>Not found</p>;
-  }
+    </div>
+  );
 }
