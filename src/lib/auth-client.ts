4cdde076b203fadc30bb8f57be1e2f16c5d29992
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-07-13 16:18:16 +0100

    unset Base url from localhost

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { adminClient } from "better-auth/client/plugins";
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
   plugins: [adminClient()],
   fetchOptions: {
     onError: async (context) => {
